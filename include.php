0ad06ffae86e2597863d326357ef78287e3331f9
Napol119 <152479370+Napol119@users.noreply.github.com>
2024-01-19 14:28:57 +0700

    Create include.php

---
+++ include.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<!-- สร้าง include -->
+                    <a href="home.php">Home |</a> <br>
+                    <a href="#">Location 1 |</a> <br>
+                    <a href="#">Location 2 |</a> <br>
+                    <a href="#">Location 3 |</a> <br>
+                    <a href="#">Location 4 |</a> <br>
+                    <a href="#">Location 5 |</a> <br>
+                     <!-- เพิ่มลิงก์ไปยังหน้า Gallery -->
+                    <a href="gallery.php">Gallery |</a> <br>
+
+                <!-- เพิ่มลิงก์ไปยังหน้า Social Media -->
+                    <a href="#">Facebook |</a>
+                    <a href="#"> Twitter |</a>
+                    <a href="#"> Instargram</a>
+ 
+</body>
+</html>
